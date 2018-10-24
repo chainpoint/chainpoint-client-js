3fe84a317efa929cfe18fd9010e7cd1969e591b0
Michael Iglesias <miglesias@tierion.com>
2018-10-24 17:38:44 -0400

    * Updated README to reflect the addition of groupId being part of the response body * Updated Function signature code documentation to reflect additional property being returned with a type of string - groupId

--- index.js
+++ index.js
@@ -410,7 +410,7 @@ function _flattenProofBranches(proofBranchArray) {
  * Submit hash(es) to one or more Nodes, returning an Array of proof handle objects, one for each submitted hash and Node combination.
  * @param {Array<String>} hashes - An Array of String Hashes in Hexadecimal form.
  * @param {Array<String>} uris - An Array of String URI's. Each hash will be submitted to each Node URI provided. If none provided three will be chosen at random using service discovery.
- * @return {Array<{uri: String, hash: String, hashIdNode: String}>} An Array of Objects, each a handle that contains all info needed to retrieve a proof.
+ * @return {Array<{uri: String, hash: String, hashIdNode: String, groupId: String}>} An Array of Objects, each a handle that contains all info needed to retrieve a proof.
  */
 function submitHashes(hashes, uris, callback) {
   uris = uris || []
@@ -526,7 +526,7 @@ function submitHashes(hashes, uris, callback) {
  * Submit hash(es) of selected file(s) to one or more Nodes, returning an Array of proof handle objects, one for each submitted hash and Node combination.
  * @param {Array<String>} paths - An Array of paths of the files to be hashed.
  * @param {Array<String>} uris - An Array of String URI's. Each hash will be submitted to each Node URI provided. If none provided three will be chosen at random using service discovery.
- * @return {Array<{path: String, uri: String, hash: String, hashIdNode: String}>} An Array of Objects, each a handle that contains all info needed to retrieve a proof.
+ * @return {Array<{path: String, uri: String, hash: String, hashIdNode: String, groupId: String}>} An Array of Objects, each a handle that contains all info needed to retrieve a proof.
  */
 function submitFileHashes(paths, uris, callback) {
   callback = callback || function() {}
