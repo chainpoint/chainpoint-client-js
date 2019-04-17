2e4e96784bcb5b65413ae02ce000827c7d4e2692
Buck Perley <bucko.perley@gmail.com>
2019-04-17 16:27:51 -0500

    Add CI, testing framework, and coverage reporting (#17)
    * add travis.yml
    
    * add mocha for tests
    
    * add coverage support
    
    * multiple jobs to run for unit tests and linting tests for clarity

---
+++ tests/bundle-test.js
@@ -0,0 +1,41 @@
+const chp = require('../dist/bundle')
+const fs = require('fs')
+const { expect } = require('chai')
+
+describe('bundle', function() {
+  this.timeout(30000)
+  let hashes, proofHandlesHashes, proofHandlesFiles, proofs
+
+  before(() => {
+    hashes = [
+      '1d2a9e92b561440e8d27a21eed114f7018105db00262af7d7087f7dea9986b0a',
+      '2d2a9e92b561440e8d27a21eed114f7018105db00262af7d7087f7dea9986b0a',
+      '3d2a9e92b561440e8d27a21eed114f7018105db00262af7d7087f7dea9986b0a'
+    ]
+  })
+
+  it('should submit each hash to three randomly selected nodes', async () => {
+    // Submit each hash to three randomly selected Nodes
+    proofHandlesHashes = await chp.submitHashes(hashes)
+    let paths = fs
+      .readdirSync('./')
+      .map(file => `./${file}`)
+      .filter(file => fs.lstatSync(file).isFile())
+
+    // Submit each hash to three randomly selected Nodes
+    proofHandlesFiles = await chp.submitFileHashes(paths)
+    expect(proofHandlesFiles).to.exist
+  })
+
+  it('should retrieve a calendar proof for each hash that was submitted', async () => {
+    // Wait for Calendar proofs to be available
+    await new Promise(resolve => setTimeout(resolve, 16000))
+    proofs = await chp.getProofs([...proofHandlesHashes, ...proofHandlesFiles])
+    expect(proofs).to.exist
+  })
+
+  it('should verify every anchor in every calendar proof', async () => {
+    const verifiedProofs = await chp.verifyProofs(proofs)
+    expect(verifiedProofs).to.exist
+  })
+})
