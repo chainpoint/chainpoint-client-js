290771ae5ee184d614eec1163973915a66a3633e
Buck Perley <bucko.perley@gmail.com>
2019-04-18 21:32:03 -0500

    move out verify and evaluate

--- index.js
+++ index.js
@@ -11,196 +11,13 @@
  * limitations under the License.
  */
 
-import _isString from 'lodash/isString'
-import _isEmpty from 'lodash/isEmpty'
-import _map from 'lodash/map'
-import _first from 'lodash/first'
-import _forEach from 'lodash/forEach'
-import _uniq from 'lodash/uniq'
-import _flatten from 'lodash/flatten'
-import _mapKeys from 'lodash/mapKeys'
-import _camelCase from 'lodash/camelCase'
-import _uniqWith from 'lodash/uniqWith'
-import _isEqual from 'lodash/isEqual'
-
-const url = require('url')
-const fetch = require('node-fetch')
-
 import utils from './lib/utils'
-import { NODE_PROXY_URI } from './lib/constants'
 import _submitHashes, { submitFileHashes as _submitFileHashes } from './lib/submit'
 import _getProofs from './lib/get'
+import _verifyProofs from './lib/verify'
+import _evaluateProofs from './lib/evaluate'
 
-const {
-  isSecureOrigin,
-  isValidNodeURI,
-  flattenBtcBranches,
-  flattenProofs,
-  normalizeProofs,
-  parseProofs,
-  promiseMap,
-  getCores,
-  getNodes
-} = utils
-
-/**
- * Verify a collection of proofs using an optionally provided Node URI
- *
- * @param {Array} proofs - An Array of String, or Object proofs from getProofs(), to be verified. Proofs can be in any of the supported JSON-LD or Binary formats.
- * @param {String} uri - [Optional] The Node URI to submit proof(s) to for verification. If not provided a Node will be selected at random. All proofs will be verified by a single Node.
- * @param {function} callback - An optional callback function.
- * @return {Array<Object>} - An Array of Objects, one for each proof submitted, with vefification info.
- */
-export function verifyProofs(proofs, uri, callback) {
-  callback = callback || function() {}
-
-  let evaluatedProofs = evaluateProofs(proofs)
-
-  // Validate and return an Array with a single Node URI
-  // if provided or get an Array of Nodes via service discovery.
-  // In both cases return a Promise.
-  let nodesPromise
-  if (_isEmpty(uri)) {
-    nodesPromise = getNodes(1)
-  } else {
-    if (!_isString(uri)) throw new Error('uri arg must be a String')
-    if (!isValidNodeURI(uri)) throw new Error(`uri arg contains invalid Node URI : ${uri}`)
-    nodesPromise = Promise.resolve([uri])
-  }
-
-  return new Promise(function(resolve, reject) {
-    try {
-      nodesPromise
-        .then(nodes => {
-          return _first(nodes)
-        })
-        .then(node => {
-          // Assign all flat proofs to the same Node URI for verification
-          let singleNodeFlatProofs = _map(evaluatedProofs, proof => {
-            let oldProofURI = url.parse(proof.uri)
-            proof.uri = node + oldProofURI.path
-            return proof
-          })
-
-          let uniqSingleNodeFlatProofs = _uniqWith(singleNodeFlatProofs, _isEqual)
-
-          return uniqSingleNodeFlatProofs
-        })
-        .then(flatProofs => {
-          let anchorURIs = []
-          _forEach(flatProofs, proof => {
-            anchorURIs.push(proof.uri)
-          })
-
-          let uniqAnchorURIs = _uniq(anchorURIs)
-
-          let nodesWithGetOpts = _map(uniqAnchorURIs, anchorURI => {
-            let headers = Object.assign(
-              {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-              },
-              isSecureOrigin()
-                ? {
-                    'X-Node-Uri': url.parse(anchorURI).protocol + '//' + url.parse(anchorURI).host
-                  }
-                : {}
-            )
-
-            let uri = isSecureOrigin() ? NODE_PROXY_URI + url.parse(anchorURI).path : anchorURI
-
-            return {
-              method: 'GET',
-              uri: uri,
-              body: {},
-              headers,
-              timeout: 10000
-            }
-          })
-
-          return [flatProofs, nodesWithGetOpts]
-        })
-        .then(([flatProofs, nodesWithGetOpts]) => {
-          // Perform parallel GET requests to all Nodes with proofs
-          let hashesByNodeURI = promiseMap(nodesWithGetOpts, fetch, {
-            concurrency: 25
-          })
-            .then(parsedBody => {
-              // promiseMap returns an Array entry for each host it submits to.
-              let flatParsedBody = _flatten(parsedBody)
-
-              let r = {}
-
-              _forEach(nodesWithGetOpts, (getOpt, index) => {
-                r[getOpt.uri] = flatParsedBody[index]
-              })
-
-              return r
-            })
-            .catch(err => {
-              reject(err)
-              return callback(err)
-            })
-
-          return [flatProofs, hashesByNodeURI]
-        })
-        .then(([flatProofs, hashesByNodeURI]) => {
-          // Fulfill the Promise for all of the request results.
-          hashesByNodeURI
-            .then(hashesFound => {
-              if (_isEmpty(hashesFound)) return Promise.reject(new Error('No hashes were found.'))
-              let results = []
-
-              _forEach(flatProofs, flatProof => {
-                if (flatProof.expected_value === hashesFound[flatProof.uri]) {
-                  // IT'S GOOD!
-                  flatProof.verified = true
-                  flatProof.verified_at = new Date().toISOString().slice(0, 19) + 'Z'
-                } else {
-                  // IT'S NO GOOD :-(
-                  flatProof.verified = false
-                  flatProof.verified_at = null
-                }
-
-                // Camel case object keys
-                let flatProofCamel = _mapKeys(flatProof, (v, k) => _camelCase(k))
-
-                results.push(flatProofCamel)
-              })
-
-              resolve(results)
-              return callback(null, results)
-            })
-            .catch(err => {
-              console.error(err.message)
-              reject(err)
-              return callback(err)
-            })
-        })
-        .catch(err => {
-          console.error(err.message)
-          reject(err)
-          return callback(err)
-        })
-    } catch (err) {
-      reject(err)
-      return callback(err)
-    }
-  })
-}
-
-/**
- * Evaluates the expected anchor values for a collection of proofs
- *
- * @param {Array} proofs - An Array of String, or Object proofs from getProofs(), to be evaluated. Proofs can be in any of the supported JSON-LD or Binary formats.
- */
-export function evaluateProofs(proofs) {
-  let normalizedProofs = normalizeProofs(proofs)
-  let parsedProofs = parseProofs(normalizedProofs)
-  let flatProofs = flattenProofs(parsedProofs)
-
-  return flatProofs
-}
+const { flattenBtcBranches, normalizeProofs, parseProofs, getCores, getNodes } = utils
 
 /**
  * retrieve raw btc tx objects for corresponding proofs
@@ -220,13 +37,15 @@ export function getProofTxs(proofs) {
 export const submitHashes = _submitHashes
 export const submitFileHashes = _submitFileHashes
 export const getProofs = _getProofs
+export const verifyProofs = _verifyProofs
+export const evaluateProofs = _evaluateProofs
 
 export default {
   getCores,
   getNodes,
-  submitHashes: _submitHashes,
-  submitFileHashes: _submitFileHashes,
-  getProofs: _getProofs,
+  submitHashes,
+  submitFileHashes,
+  getProofs,
   verifyProofs,
   evaluateProofs,
   getProofTxs
