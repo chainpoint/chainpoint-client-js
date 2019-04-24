aa3779600923012382c7d73f07eb6c2748a62c4b
Buck Perley <bucko.perley@gmail.com>
2019-04-23 22:59:16 -0500

    stub submit and verify tests

---
+++ tests/submit-test.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2019 Tierion
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// import { expect } from 'chai'
+
+// import { submitHashes, submitFileHashes } from '../index'
+
+describe('submitHashes', () => {
+  it('should reject invalid hashes arg')
+  it('should reject invalid uris arg')
+  it('should get node uris from a core if none are passed')
+  it('should send POST request to nodes with hashes in the request body')
+  it('should return proof handles after successful submission')
+
+  describe('submitFileHashes', () => {
+    it('should get hashes of files at given paths')
+    it('should run submitHashes')
+    it('should return proofHandles with path property matching the path of the submitted hash')
+  })
+})
